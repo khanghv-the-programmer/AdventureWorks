0dd2f11c52bb07e57991fa8762f1c9a13c927e5f
khanghv-the-programmer <khanghvse140097@fpt.edu.vn>
2021-02-26 22:24:17 +0700

    1st: the first upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using AdventureWorksBeta.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureWorksBeta.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private AdventureWorks2014Context _db;
+
+        public HomeController(ILogger<HomeController> logger, AdventureWorks2014Context db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult People()
+        {
+            List<Person> people = _db.Person.ToList();
+            return View(people);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Welcome to khanghv repository
+This repo contains the source code of a C# core (3.1) application used to manage a shop based on "AdventureWorks" database of Microsoft 
+
+## You will find in this repo:
+* AdventuresWorks Webpage (developing)
+* src code folder
+
+## Connect me via
+1. [GitHub](https://github.com/khanghv-the-programmer)
+2. [Facebook](https://www.facebook.com/huavinhkhangfd3600)
+3. [Email](mail: khanghvse140097@fpt.edu.vn)
+#### © 2020 by khanghv
